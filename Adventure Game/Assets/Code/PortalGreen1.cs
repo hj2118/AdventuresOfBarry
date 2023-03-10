1fa0a1dd46f7a0d8f4e18f873e387e0d08ce058a
Ashley <afl331@nyu.edu>
2023-03-10 16:46:09 -0500

    Update lvl5

--- Adventure Game/Assets/Code/PortalGreen1.cs
+++ Adventure Game/Assets/Code/PortalGreen1.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PortalGreen1 : MonoBehaviour
 {
-
     public GameObject Player;
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
     Vector3 newPos = new Vector3(-2.47f, 44.194f, 1.15f);
+
 
     void OnTriggerEnter(Collider other){
         if (other.CompareTag("Player")){
-            Player.transform.position = newPos;
+            print("hit");
+            Player1.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+            Player3.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+            Player1.SetActive(true);
+            Player3.SetActive(false);
         }
     }
+
+    // void OnTriggerEnter(Collider other){
+    //     if (other.CompareTag("Player")){
+    //         Player.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+    //         Player.GetComponent<UnityEngine.AI.NavMeshAgent>().ResetPath();
+    //         Player.transform.position = newPos;
+    //         print("hit");
+    //         Invoke("StartNavMesh", 1f);
+    //     }
+    // }
+
+    // private void StartNavMesh(){
+    //     Player.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+    // }
 }
--- Adventure Game/Assets/Code/PortalPurple1.cs
+++ Adventure Game/Assets/Code/PortalPurple1.cs
@@ -6,12 +6,46 @@ public class PortalPurple1 : MonoBehaviour
 {
 
     public GameObject Player;
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
     Vector3 newPos = new Vector3(-1.586f, 44.194f, 8.428f);
+
+    void Start(){
+            Player.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+            Player1.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+            Player2.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+            Player3.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+            Player4.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+            Player.SetActive(true);
+            Player1.SetActive(false);
+            Player2.SetActive(false);
+            Player3.SetActive(false);
+            Player4.SetActive(false);
+    }
+
 
     void OnTriggerEnter(Collider other){
         if (other.CompareTag("Player")){
-            Player.transform.position = newPos;
-            print("hit");
+            Player.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+            Player2.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+            Player.SetActive(false);
+            Player2.SetActive(true);
         }
     }
+
+    // void OnTriggerEnter(Collider other){
+    //     if (other.CompareTag("Player")){
+    //         Player.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+    //         Player.GetComponent<UnityEngine.AI.NavMeshAgent>().ResetPath();
+    //         Player.transform.position = newPos;
+    //         print("hit");
+    //         Invoke("StartNavMesh", 1f);
+    //     }
+    // }
+
+    // private void StartNavMesh(){
+    //     Player.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+    // }
 }
